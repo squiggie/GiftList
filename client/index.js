b2f7c06849d87f89bd35cf79b3a2fd748bc41c19
Squiggie <squigge@gmail.com>
2023-10-25 02:31:55 +0000

    Update 5 files

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+  const merkleTree = new MerkleTree(niceList);
+  const name = "Norman Blocke";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  await axios
+    .post(`${serverUrl}/gift`, {
+      params: { proof: proof, name: name },
+    })
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
 main();
